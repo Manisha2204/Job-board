b8f399a04b4eff913cd664e33106f83dfe25e384
Manisha2204 <118578660+Manisha2204@users.noreply.github.com>
2025-07-11 09:37:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+document.getElementById('jobForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(this);
+  const job = {};
+  formData.forEach((value, key) => {
+    job[key] = value.trim();
+  });
+
+  fetch('https://script.google.com/macros/s/AKfycbxNFYq1Tr18w1iVjl-9V2PVYiELmpZI39qVkSyA9OR3bLhUAJd_QbGdS8c28406HTrWqA/exec', {
+    method: 'POST',
+    body: JSON.stringify(job),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.text())
+    .then(data => {
+      alert('Job posted successfully!');
+      document.getElementById('jobForm').reset();
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      alert('Failed to post job. Try again.');
+    });
+});
 
